8e43d625fcbeb1da7e9615d02d70ba75cf39834d
BT_2N2_07 <sample@bths.edu>
2023-12-15 13:47:12 -0500

    Second

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("ello");
+        for (int i = 0; i < 3; i++) {
+            System.out.println(i);
+        }
     }
 }
